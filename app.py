fab67c0480fdcc40566a0a0acdeb9732b085b225
shreyas_bhat <rbhatshreyas@gmail.com>
2020-04-19 12:52:54 +0530

    Managing resource using rest

--- app.py
+++ app.py
@@ -1,9 +1,60 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello World"
+stores = [
+    {
+        'name': "MY_store",
+        'items':[{
+            'name': 'item1',
+            'price': 100,
+        }]
+    }
+]
 
-app.run(port=5000)
+# POST /store data: {name:}
+@app.route('/store', methods=["POST"])
+def create_store():
+    request_store = request.get_json()
+    new_store = {
+        'name': request_store["name"],
+        'items': [] 
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+# GET /store/<string:name> 
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({"store": stores})
+
+#POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=["POST"])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+            }
+            store["items"].append(new_item)
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+#GET /store/<string:name>/item
+
+@app.route('/store/<string:name>/item', methods=["GET"])
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store["items"])
+    return jsonify({"no item found"})
+
+app.run(port=5000, debug=True)
