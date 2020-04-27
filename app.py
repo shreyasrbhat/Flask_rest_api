1c7e91207568d46cc04a94445205d6fe07147849
shreyas_bhat <rbhatshreyas@gmail.com>
2020-04-27 22:17:16 +0530

    feature: authentication

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse 
 from security import authenticate, identity
 from flask_jwt import JWT, jwt_required
 
@@ -18,10 +18,15 @@ class Item(Resource):
         return {"item":item}, 200 if item is not None else 404
     
     def post(self, name):
+        parser = reqparse.RequestParser()
+        #parser.add_argument('name', type=str, required=True)
+        parser.add_argument('price', type=float, required=True)
+        data = parser.parse_args()
         item = next(filter(lambda x: x == name, items), None)
         if item:
             return {"message": f"item {name} already exist"} , 404
-        data = request.get_json()
+        #
+        # data = request.get_json()
         item = {"name": name,
                  "price": data["price"]}
         items.append(item)
